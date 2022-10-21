27192171d24e7e76f266b934a15eb92ae5a703a7
Dan Heslinga <drdan@heslingamd.com>
2022-10-21 13:49:34 -1000

    try console.log

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -25,6 +25,7 @@
         $.when(pt, obv).fail(onError);
 
         $.when(pt, obv).done(function(patient, obv) {
+          window.console&&console.log('foo'); // DH
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
